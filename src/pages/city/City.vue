2c4743884371585dd6e0e1c22c789259d50fc1b7
JustStudy <2286981662@qq.com>
2019-02-11 15:05:16 +0800

    add CitySearch

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
       <CityHeader />
+      <CitySearch />
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'city',
   components: {
-    CityHeader: CityHeader
+    CityHeader: CityHeader,
+    CitySearch: CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Search">
+    <input type="text" placeholder="请输入城市名称" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~styles/sassreset.scss";
+  .Search{
+    text-align: center;
+    background: $HeaderBgColor;
+    input{
+      width: 100%;border:$HeaderBgColor .1rem solid;
+      @include box-sizing;
+      @include border-radius(.2rem);
+      line-height: .5rem;
+      text-align: center;
+    }
+  }
+</style>
