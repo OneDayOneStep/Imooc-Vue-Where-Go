1ec63f34255554153dc005ebbfc3d0d24aa8fc72
JustStudy <2286981662@qq.com>
2019-02-13 15:05:49 +0800

    Search City

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="Search">
-    <input type="text" placeholder="请输入城市名称" />
+  <div class='Search'>
+    <input type='text' placeholder='请输入城市名称' v-model='SearchData' />
+    <div class='SearchContent'>
+      <div v-for="(city, index) of Citys" v-show="city.indexOf(SearchData) >= 0 && SearchData !== ''" :key="'city_' + index">{{ city }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      SearchData: '',
+      Citys: ['广州', '日本', '上海', '北京', '美国', '韩国', '广东']
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
   @import "~styles/sassreset.scss";
   .Search{
     text-align: center;
@@ -21,6 +30,19 @@ export default {
       @include border-radius(.2rem);
       line-height: .5rem;
       text-align: center;
+    }
+  }
+  .SearchContent{
+    position: absolute;
+    left: 0;
+    right: 0;
+    background: #FFF;
+    z-index: 5;
+    @include box-shadow(0 1px 2px #888);
+    div{
+      line-height: .6rem;
+      border-bottom: #EEE 1px solid;
+      color: #666;
     }
   }
 </style>
